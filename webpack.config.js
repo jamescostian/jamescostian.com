7302280cddf145136dd48b15aab65ab18d21f6e1
James Costian <james@jamescostian.com>
2017-05-05 12:16:30 -0500

    Start using webpack for JS and CSS

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const {join} = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+module.exports = {
+  entry: [
+    join(__dirname, 'css', 'main.css'),
+    join(__dirname, 'js', 'main.js')
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: __dirname
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          use: {
+            loader: 'css-loader',
+            options: {
+              minimize: true,
+              sourceMap: true
+            }
+          }
+        })
+      }
+    ]
+  },
+  devtool: 'source-map',
+  plugins: [
+    new (require('babili-webpack-plugin'))(),
+    new ExtractTextPlugin('bundle.css')
+  ]
+}
