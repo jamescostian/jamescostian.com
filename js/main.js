288738326ed994cea05b49f4f042eb4014bc114c
James Costian <james@jamescostian.com>
2017-05-21 21:56:53 -0500

    No more JS, everything will be static

--- js/main.js
+++
@@ -1,4 +0,0 @@
-// TODO: don't just assume this is the homepage, account for the fact that the user might be visiting #blog/something
-// And don't forget to window.addEventListener('hashchange', () => {}, false)
-import setupBlogPostPreviews from './blog-post-previews.js'
-setupBlogPostPreviews()
